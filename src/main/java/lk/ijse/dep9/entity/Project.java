d88313971a140d9d580da4a1a0d02171f697e73b
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2023-01-01 21:32:38 +0530

    Call back to jdbc

--- src/main/java/lk/ijse/dep9/entity/Project.java
+++ src/main/java/lk/ijse/dep9/entity/Project.java
@@ -9,24 +9,9 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@ToString(exclude = "taskSet")
-@EqualsAndHashCode(exclude = "taskSet")
 public class Project implements SuperEntity{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(nullable = false)
     private String name;
-    @ManyToOne
-    @JoinColumn(name = "username", referencedColumnName = "username",nullable = false)
     private User user;
-    @OneToMany (mappedBy = "project",fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
-    @Setter(AccessLevel.NONE)
-    private Set<Task> taskSet = new HashSet<>();
 
-    public Project(String name, User user) {
-        this.name = name;
-        this.user = user;
-    }
 }
