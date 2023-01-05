0724238502fe9a2dc1a6a927e4fe0b3fc3a9b701
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2023-01-05 06:44:58 +0530

    Add UserController

--- src/main/java/lk/ijse/dep9/app/api/UserController.java
+++ src/main/java/lk/ijse/dep9/app/api/UserController.java
@@ -4,13 +4,15 @@ import lk.ijse.dep9.app.dto.UserDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/users")
 @CrossOrigin
 public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "application/json")
-    public void createUserAccount(@RequestBody UserDTO userDTO){
+    public void createUserAccount(@Valid @RequestBody UserDTO userDTO){
         System.out.println("user");
     }
 
