3b2893db244b360870b47927807c447b938a156b
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2023-01-06 09:54:01 +0530

    Handle validation exception smoothly

--- src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
+++ src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
@@ -1,24 +1,25 @@
 package lk.ijse.dep9.app.advice;
 
-import lk.ijse.dep9.app.dto.ErrorResponseMsgDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler{
-    @ExceptionHandler(Throwable.class)
-    public ErrorResponseMsgDTO uncaughtException(Throwable t){
-        t.printStackTrace();
-        return new ErrorResponseMsgDTO(t.getMessage(),405);
-    }
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponseMsgDTO validationException(MethodArgumentNotValidException e){
-        String message= e.getFieldErrors().stream().map(fieldError -> fieldError.getField()+":"+fieldError.getDefaultMessage()+",")
-                .reduce((prev, current)-> prev+current).get();
-        return new ErrorResponseMsgDTO(message, 400);
+    public Map<String, Object> validationException(MethodArgumentNotValidException e){
+        HashMap<String, Object> errAttribute = new LinkedHashMap<>();
+        errAttribute.put("status", HttpStatus.BAD_REQUEST.value());
+        errAttribute.put("error",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        errAttribute.put("timestamp", new Date().toString());
+        List<String> validationList = e.getFieldErrors().stream().map(err -> err.getField() + ":" + err.getDefaultMessage()).collect(Collectors.toList());
+        errAttribute.put("errors",validationList);
+        return  errAttribute;
     }
 }
