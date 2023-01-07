326a9ea87b5f1417146446db49588b6a8a627c35
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2023-01-07 11:13:54 +0530

    Refer the jdbc connection pool inside the web xml

--- src/main/java/lk/ijse/dep9/app/service/ServiceFactory.java
+++ src/main/java/lk/ijse/dep9/app/service/ServiceFactory.java
@@ -1,5 +1,9 @@
 package lk.ijse.dep9.app.service;
 
+import lk.ijse.dep9.app.service.custom.UserService;
+import lk.ijse.dep9.app.service.custom.impl.ProjectTaskImpl;
+import lk.ijse.dep9.app.service.custom.impl.UserServiceImpl;
+
 public class ServiceFactory {
 
     private static ServiceFactory serviceFactory;
@@ -9,5 +13,16 @@ public class ServiceFactory {
 
     public static ServiceFactory getInstance(){
         return (serviceFactory==null)?(serviceFactory=new ServiceFactory()): serviceFactory;
+    }
+
+    public <T extends SuperService> T getService(ServiceTypes serviceTypes, Class<T> tClass){
+        switch (serviceTypes){
+            case USER:
+                return tClass.cast(new UserServiceImpl());
+            case PROJECT_TASK:
+                return tClass.cast(new ProjectTaskImpl());
+            default:
+                return null;
+        }
     }
 }
