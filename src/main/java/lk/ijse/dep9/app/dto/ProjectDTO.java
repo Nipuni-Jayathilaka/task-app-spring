4c3180932438376e45f8e38300074df9bef08944
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2023-01-22 08:11:13 +0530

    Add ProjectDTO

---
+++ src/main/java/lk/ijse/dep9/app/dto/ProjectDTO.java
@@ -0,0 +1,22 @@
+package lk.ijse.dep9.app.dto;
+
+import lk.ijse.dep9.app.util.ValidationGroups;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectDTO {
+    @Null(groups = ValidationGroups.Create.class, message = "Project id cannot be not null")
+    private Integer id;
+    @NotNull(message = "project name cannot be null")
+    @Length(min = 3,message = "Project name should be at least three characters")
+    private String name;
+}
