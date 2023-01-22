6493d1f2e72698055275a80a8ba0a56e43f7e32d
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2023-01-22 08:08:29 +0530

    Finalize the project controller structure

--- src/main/java/lk/ijse/dep9/app/api/ProjectController.java
+++ src/main/java/lk/ijse/dep9/app/api/ProjectController.java
@@ -1,7 +1,19 @@
 package lk.ijse.dep9.app.api;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1/projects")
 @RestController
+@CrossOrigin
 public class ProjectController {
+
+    private void createNewProject(){}
+    private void getAllProject(){}
+    private void getProject(){}
+    private void renameProject(){}
+    private void deleteProject(){}
+
 }
